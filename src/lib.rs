e81529eefeada0a8037d8312a83c413b6d98b735
Illyrius <FitimQ@live.nl>
2026-03-23 13:25:21 +0100

    some more adjustments
    Signed-off-by: Illyrius <FitimQ@live.nl>

--- src/lib.rs
+++ src/lib.rs
@@ -20,19 +20,19 @@ use crate::player::Player;
 use pumpkin_plugin_api::{Context, PluginMetadata};
 use tracing::info;
 
-pub const PLUGIN_ID: &str = "vanillaplus";
+pub const PLUGIN_ID: &str = "pumpkinplus";
 
 /// IllyriaPlus plugin implementation.
-pub struct IllyriaPlus {}
+pub struct PumpkinPlus {}
 
-impl IllyriaPlus {
+impl PumpkinPlus {
     fn new() -> Self {
-        IllyriaPlus {}
+        PumpkinPlus {}
     }
 
     fn metadata(&self) -> PluginMetadata {
         PluginMetadata {
-            name: "Vanilla+".into(),
+            name: "Pumpkin+".into(),
             version: env!("CARGO_PKG_VERSION").into(),
             authors: vec!["Xodium".into()],
             description: "Minecraft plugin that enhances the base gameplay in Rust".into(),
@@ -42,12 +42,12 @@ impl IllyriaPlus {
     fn on_load(&mut self, context: Context) -> pumpkin_plugin_api::Result<()> {
         Motd::default().register(&context);
         Player::default().register(&context);
-        info!("IllyriaPlus loaded. NICE TO CYA!");
+        info!("Pumpkin+ loaded. NICE TO CYA!");
         Ok(())
     }
 
     fn on_unload(&mut self, _context: Context) -> pumpkin_plugin_api::Result<()> {
-        info!("IllyriaPlus unloaded. CYA NEXT TIME!");
+        info!("Pumpkin+ unloaded. CYA NEXT TIME!");
         Ok(())
     }
 }
